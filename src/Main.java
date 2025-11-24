6eecee5b42f31f3b10db0f9cd7731728c3a6425e
Gabriel_Noleto <gabrielsiqueirarn@gmail.com>
2025-11-24 12:33:00 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.lang.management.MemoryNotificationInfo;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
+        Curso curso1 = new Curso();
+            curso1.setTitulo("Curso Java");
+            curso1.setDescricao("Descrição curso java");
+            curso1.setCargaHoraria(8);
+            System.out.println(curso1);
+            System.out.println("=============================");
 
-        System.out.println("Estou aqui");
+        Curso curso2 = new Curso();
+            curso2.setTitulo("Curso Kotlin");
+            curso2.setDescricao("Descrição curso Kotlin");
+            curso2.setCargaHoraria(16);
+            System.out.println(curso2);
+            System.out.println("=============================");
+
+
+        Mentoria mentoria = new Mentoria();
+            mentoria.setTitulo("Mentoria java");
+            mentoria.setDescricao("Descrição mentoria java");
+            mentoria.setData(LocalDate.now());
+
+            System.out.println(mentoria);
+            System.out.println("===============================");
+
+
+
         }
     }
